0bbb85aaf451e657cfbe7cf205e90abe609d5dc2
LUCDADESIGN <144266913+LUCDADESIGN@users.noreply.github.com>
2023-10-12 20:13:40 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
+
+let tasks = [];
+
+// Create
+app.post('/tasks', (req, res) => {
+  const task = req.body;
+  tasks.push(task);
+  res.json({ message: 'Task added successfully', task });
+});
+
+// Read
+app.get('/tasks', (req, res) => {
+  res.json(tasks);
+});
+
+// Update
+app.put('/tasks/:id', (req, res) => {
+  const id = req.params.id;
+  const updatedTask = req.body;
+
+  tasks = tasks.map(task => (task.id === id ? updatedTask : task));
+  res.json({ message: 'Task updated successfully', tasks });
+});
+
+// Delete
+app.delete('/tasks/:id', (req, res) => {
+  const id = req.params.id;
+  tasks = tasks.filter(task => task.id !== id);
+  res.json({ message: 'Task deleted successfully', tasks });
+});
