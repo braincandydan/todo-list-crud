6392b0b21150e3a297519bd0e1c210fcd3d222e7
LUCDADESIGN <144266913+LUCDADESIGN@users.noreply.github.com>
2023-10-12 20:37:27 -0700

    commit 4

--- script.js
+++ script.js
@@ -1,18 +1,20 @@
 function addTask() {
     const taskName = document.getElementById('newTask').value;
     const dueDate = document.getElementById('dueDate').value;
+    const dueTime = document.getElementById('dueTime').value;
   
     if (taskName.trim() !== '') {
       const taskList = document.getElementById('taskList');
       const li = document.createElement('li');
       li.innerHTML = `
         <span>${taskName}</span>
-        <span>Due: ${formatDueDate(dueDate)}</span>
+        <span>Due: ${formatDueDate(dueDate, dueTime)}</span>
         <button onclick="deleteTask(this)">Delete</button>
       `;
       taskList.appendChild(li);
       document.getElementById('newTask').value = '';
       document.getElementById('dueDate').value = '';
+      document.getElementById('dueTime').value = '';
     }
   }
   
@@ -20,8 +22,8 @@ function addTask() {
     button.parentElement.remove();
   }
   
-  function formatDueDate(dateString) {
+  function formatDueDate(dateString, timeString) {
     const options = { year: 'numeric', month: 'long', day: 'numeric', hour: 'numeric', minute: 'numeric', second: 'numeric' };
-    return new Date(dateString).toLocaleDateString('en-US', options);
+    return new Date(`${dateString}T${timeString}`).toLocaleDateString('en-US', options);
   }
   
